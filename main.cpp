3ffcd80be96d016d02d81ce992339981e5a19b69
ChronosDJ <chronosdj2901@gmail.com>
2021-06-07 03:49:16 +0300

    First

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <C:\Users\CDJ\Desktop\8-lab\sdt.h>
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("\n Enter the first number:");
+    scanf("%d",&a);
+    printf("\n Enter the second number:");
+    scanf("%d",&b);
+    return 0;
+}
